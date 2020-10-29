b441607c62d29651b7b15cd84802aa2efacad930
Rocky <yangrocky2013@gmail.com>
2020-10-28 19:51:08 -0500

    add questions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AppContext } from './context/AppContext'
 import Splash from './components/pages/Splash'
+import json from "./assets/Apprentice_TandemFor400_Data.json"
 
 function App() {
+  const [questions, setQuestions] = useState(json)
+  
   return (
     <BrowserRouter>
       {/* <AppContext.Provider value={{  }}> */}
--- src/components/pages/Splash.js
+++ src/components/pages/Splash.js
@@ -1,7 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory} from "react-router-dom";
 
 const Splash = () => {
+    
+    const fetchQuestions = async () => {
+        // const res = await fetch('../../assets/Apprentice_TandemFor400_Data.json')
+        // .then(response => {
+        //     console.log(response.json())
+        // })
+        // .then(res => {
+        //     console.log(res)
+        // })
+        // const body = await res.json()
+    }
+
+    useEffect(() => {
+        fetchQuestions()
+    }, [])
 
     return (
         <div className="splash-page css-typing">
